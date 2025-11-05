cbb4e618a6dc6d8a54aa0ba05bca506e58678fd3
Zap <oscarzapt1@gmail.com>
2025-11-05 13:51:55 -0600

    Refactor player stats retrieval logic

--- input.py
+++ input.py
@@ -10,5 +10,5 @@ for line in infoPrint:
     if line.startswith(user):
         player_name = line.split("\t")[1]
         inp.write("\nRookie no. " + str(user) + " " + player_name + "\n")
-        inp.write("date of recent game: " + line.split("\t")[2] + "\n" + "team: " + line.split("\t")[3] + "\n" + "opponent: " + line.split("\t")[4] + "\n" + "W/L: " + line.split("\t")[5] )
+        inp.write("date of recent game: " + line.split("\t")[2] + "\n" + "team: " + line.split("\t")[3] + "\n" + "opponent: " + line.split("\t")[4] + "\n" + "W/L: " + line.split("\t")[5] + "\n" + "score: " + line.split("\t")[6] )
         break
