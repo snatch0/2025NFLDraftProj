4815dead1f44f96ddc0fd94b6f7449fcbe23dc22
Zap <oscarzapt1@gmail.com>
2025-11-25 14:27:26 -0600

    Update GUIVersionInput.py

--- GUIVersionInput.py
+++ GUIVersionInput.py
@@ -5,13 +5,22 @@ random_img = r"C:\Users\zapata1498\Desktop\python\unknown-person-hidden-covered-
 app = customtkinter.CTk()
 userDraftInput = customtkinter.StringVar()
 plrName= customtkinter.StringVar()
-date_recent_game = customtkinter.StringVar()
-team = customtkinter.StringVar()
-opponent = customtkinter.StringVar()
-winOrLose = customtkinter.StringVar()
-score_ofRecentGame = customtkinter.StringVar()
+date_recent_gameA = customtkinter.StringVar()
+teamA = customtkinter.StringVar()
+opponentA = customtkinter.StringVar()
+winOrLoseA = customtkinter.StringVar()
+score_ofRecentGameA = customtkinter.StringVar()
 globalDraftNumberVariable = customtkinter.StringVar()
+imgSlot = customtkinter.StringVar()
+imgSlot.set(random_img)
+def changeImg():
+    print("image changed")
+    
+    
+    
+
 def returnPlrInfo():
+    changeImg()
     draft_number = draftInput.get()
     
     print(draft_number)
@@ -20,7 +29,7 @@ def returnPlrInfo():
     for line in plr_info_file:
         if line.startswith(draft_number):
             player_name = line.split("\t")[1]
-            globalDraftNumberVariable = line.split("\t")[0]
+            draftN = line.split("\t")[0]
             date_recent_game = line.split("\t")[2]
             team = line.split("\t")[3]
             opponent = line.split("\t")[4]
@@ -34,12 +43,12 @@ def returnPlrInfo():
             print(winOrLose)
             print(score_ofRecentGame)
             plrName.set("Name: " + player_name + "\n")
-            globalDraftNumberVariable.set("Draft Number: " + globalDraftNumberVariable + "\n")
-            date_recent_game.set("Date of Recent Game: " + date_recent_game + "\n")
-            team.set("Team: " + team + "\n")
-            opponent.set("Opponent: " + opponent + "\n")    
-            winOrLose.set("W/L: " + winOrLose + "\n")
-            score_ofRecentGame.set("Score of Recent Game: " + score_ofRecentGame + "\n")
+            globalDraftNumberVariable.set("Draft Number: " + draftN + "\n")
+            date_recent_gameA.set("Date of Recent Game: " + date_recent_game + "\n")
+            teamA.set("Team: " + team + "\n")
+            opponentA.set("Opponent: " + opponent + "\n")    
+            winOrLoseA.set("W/L: " + winOrLose + "\n")
+            score_ofRecentGameA.set("Score of Recent Game: " + score_ofRecentGame + "\n")
             break
 
 
@@ -58,11 +67,11 @@ corner_radius=15
 
 
 
-pil_image = Image.open(random_img)
+pil_image = Image.open(random_img) #base image opened
 
-randomImgUpload = customtkinter.CTkImage(light_image=pil_image, size=(150, 200))
+randomImgUpload = customtkinter.CTkImage(light_image=pil_image, size=(150, 200)) #resize image for CTk
 
-profileLabel = customtkinter.CTkLabel(app, image=randomImgUpload, text="")
+profileLabel = customtkinter.CTkLabel(app, image=imgSlot, text="") #image label
 
 
 askLabel = customtkinter.CTkLabel(app, text="Enter Draft Number:", font=("Impact", 20), text_color="white")
@@ -82,19 +91,19 @@ plrNameLabel = customtkinter.CTkLabel(app, text=plrName.get(), font=("Impact", 2
 draftNumberLabel = customtkinter.CTkLabel(app, text=globalDraftNumberVariable.get(), font=("Impact", 20), text_color="white", textvariable=globalDraftNumberVariable)
 
 
-dateGameLabel = customtkinter.CTkLabel(app, text=date_recent_game.get() ,font=("Impact", 20), text_color="white", textVariable =date_recent_game)
+dateGameLabel = customtkinter.CTkLabel(app, text=date_recent_gameA.get(), font=("Impact", 20), text_color="white", textvariable = date_recent_gameA)
 
 
-teamLabel = customtkinter.CTkLabel(app, text=team.get(), font=("Impact", 20), text_color="white", textVariable=team)
+teamLabel = customtkinter.CTkLabel(app, text=teamA.get(), font=("Impact", 20), text_color="white", textvariable=teamA)
 
 
-opponentLabel = customtkinter.CTkLabel(app, text=opponent.get(), font=("Impact", 20), text_color="white", textVariable=opponent)
+opponentLabel = customtkinter.CTkLabel(app, text=opponentA.get(), font=("Impact", 20), text_color="white", textvariable=opponentA)
 
 
-winOrLoseLabel = customtkinter.CTkLabel(app, text=winOrLose.get(), font=("Impact", 20), text_color="white", textVariable=winOrLose)
+winOrLoseLabel = customtkinter.CTkLabel(app, text=winOrLoseA.get(), font=("Impact", 20), text_color="white", textvariable=winOrLoseA)
 
 
-scoreLabel = customtkinter.CTkLabel(app, text=score_ofRecentGame.get(), font=("Impact", 20), text_color="white", textVariable = score_ofRecentGame)
+scoreLabel = customtkinter.CTkLabel(app, text=score_ofRecentGameA.get(), font=("Impact", 20), text_color="white", textvariable = score_ofRecentGameA)
 
 topLabel.grid(row=0, column=2, padx=20, pady=20, sticky="nsew")
 
@@ -102,7 +111,7 @@ askLabel.grid(row=1, column=0, padx=20, pady=20, sticky="w")
 draftInput.grid(row=1, column=1, padx=20, pady=20, sticky="w")
 findButton.grid(row=1, column=2, padx=20, pady=20, sticky="w")
 
-profileLabel.grid(row=3, column=0, rowspan=6,  padx=20, pady=20, sticky="nsew")
+profileLabel.grid(row=3, column=0, rowspan=6,  padx=20, pady=20, sticky="nsew") #image label grid
 
 rookieLabel.grid(row=3, column=1, padx=20, pady=20, sticky="w")
 plrNameLabel.grid(row=3, column=1, padx=20, pady=20, sticky="nw")
